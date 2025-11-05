05ea9b1841784a5491d5b56dd2341f41cd916204
Aaron <184453194+aeon45525@users.noreply.github.com>
2025-11-05 19:22:56 +0800

    no understand

--- arrayMethods/script.js
+++ arrayMethods/script.js
@@ -1,22 +1,21 @@
-function sumOfTripledEvens(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    // Step 1: If the element is an even number
-    if (array[i] % 2 === 0) {
-      // Step 2: Multiply this number by three
-      const tripleEvenNumber = array[i] * 3;
-
-      // Step 3: Add the new number to the total
-      sum += tripleEvenNumber;
-    }
-  }
-  return sum;
+// Translate border-left-width to borderLeftWidth
+function camelize(str) {
+  return str
+    .split('-') 
+    .map(
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); 
 }
+// console.log(camelize("background-color"))
 
-// solution
-function sumOfTripledEvens(array) {
-  return array
-    .filter((num) => num % 2 === 0)
-    .map((num) => num * 3)
-    .reduce((acc, curr) => acc + curr);
-}
+
+// Filter range
+
+
+
+// Filter range "in place"
+// Sort in decreasing order
+// Copy and sort array
+// Shuffle an array
+// Filter unique array members
